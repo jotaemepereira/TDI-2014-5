118c80d15854398f301c9bc4792c804a3d104aed
im4184293 <im4184293@926869Lx.(none)>
2014-08-27 10:07:58 -0300

    mongo

--- HeatMeAppServer/serverHeatMe.js
+++ HeatMeAppServer/serverHeatMe.js
@@ -1,12 +1,13 @@
 
-var express = require('express');
-var url = require('url');
+var express     = require('express');
+var url         = require('url');
+var bodyParser  = require('body-parser');
+var mongo       = require('mongodb');
+var mongoose    = require('mongoose');
 
 var app = express();
 
 app.use(express.urlencoded());
-
-var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded());
 
@@ -14,33 +15,58 @@ var server = app.listen(8080, function(){
     console.log('heatMe is listening on port %d', server.address().port);
 });
 
+mongoose.connect('mongodb://localhost/heatMe');
+
+var Position = mongoose.model('Position', { latitud: String, longitud: String, date:  { type: Date, default: Date.now } });
+
+
+//<PRUEBA>
+
+//guardar en la db
+var pos = new Position({ latitud: '11', longitud: '22'/*, timestamp: req.body.timestamp */});
+pos.save(function(err){
+
+    if (err) // ...
+        console.log('Error recording position');
+    else
+        console.log('Position recorded');
+
+    Position.find({"date": {'$gte': new Date('3/1/2014'), '$lt': new Date('3/16/2015')}}, function(err, docs){
+        console.log(docs);
+    });
+
+});
+
+//</PRUEBA>
+
 app.post('/locationUpdate', function(req, res){
 
     console.log('POST /locationUpdate');
 
-    console.log(req.body.timestamp);
+    //console.log(req.body.timestamp);
     console.log(req.body.latitud);
     console.log(req.body.longitud);
 
     //guardar en la db
+    var pos = new Position({ latitud: req.body.latitud, longitud: req.body.longitud/*, timestamp: req.body.timestamp */});
+    pos.save(function(err){
 
+        if (err) // ...
+            console.log('Error recording position');
+        else
+            console.log('Position recorded');
 
+    });
 
 });
-
 
 app.get('/getHeatMap', function(req, res){
 
     console.log('GET /getHeatMap');
 
-    console.log(req.body.timestamp);
-    console.log(req.body.latitud);
-    console.log(req.body.longitud);
-
-    console.log(req.url);
     var urlpars = url.parse(req.url,false,true);
 
-    var params = urlpars.query.match(/timestamp=(.+)&latitud=(.+)&longitud=(.+)/) ;
+    var params = urlpars.query.match(/timestamp=(.+)&latitud=(.+)&longitud=(.+)/);
 
     var timestamp = params[1];
     var latitud= params[2];
@@ -50,9 +76,8 @@ app.get('/getHeatMap', function(req, res){
     console.log(latitud);
     console.log(longitud);
 
-
-    //guardar en la db
-
-
+    Position.find({"date": {'$gte': new Date('3/1/2014'), '$lt': new Date('3/16/2015')}}, function(err, docs){
+        console.log(docs);
+    });
 
 });
