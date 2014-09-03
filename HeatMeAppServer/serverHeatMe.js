0286626aa9c47f6f1f556f1646e8491d4ad01328
Agustin Prado <aguprado@gmail.com>
2014-09-03 20:24:51 -0300

    lalala

--- HeatMeAppServer/serverHeatMe.js
+++ HeatMeAppServer/serverHeatMe.js
@@ -55,7 +55,7 @@ app.post('/locationUpdate', function(req, res){
 
     console.log(req.body.latitud);
     console.log(req.body.longitud);
-    console.log(req.body.timestamp);
+    console.log(new Date(req.body.timestamp));
     console.log(req.body.tipo);
 
     //guardar en la db
